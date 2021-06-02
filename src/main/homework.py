863544d2b5c87485c2d5806de75d97a61a91a344
Brock Stechschulte <stechsbj@mail.uc.edu>
2021-06-01 21:31:22 -0400

    Homework 1

--- src/main/homework.py
+++ src/main/homework.py
@@ -9,11 +9,7 @@ def problem_1():
     bengals = Team("Bengals", "Cincinnati")
     dolphins = Team("Dolphins", "Miami")
 
-    # TODO: Add New NFL Teams
-    jets = Team("Jets", "New York")
-    jaguars = Team("Jaguars", "Jacksonville")
-    seahawks = Team("Seahawks", "Seattle")
-    packers = Team("Packers", "Green Bay")
+    # TODO: (1) Add New NFL Teams
 
     # Set Schedules
     bears.create_schedule([browns, bengals, dolphins])
@@ -21,19 +17,14 @@ def problem_1():
     bengals.create_schedule([dolphins, bears, browns])
     dolphins.create_schedule([bengals, browns, bears])
 
-    # TODO: Set New Team's Schedule
-    jets.create_schedule([jaguars, seahawks, packers])
-    jaguars.create_schedule([jets, packers, seahawks])
-    seahawks.create_schedule([packers, jets, jaguars])
-    packers.create_schedule([seahawks, jaguars, jets])
+    # TODO: (2) Set New Team's Schedule
 
     # Initialize League
-    nfl = League([bears, browns, bengals, dolphins, jets, jaguars, seahawks, packers])
+    nfl = League([bears, browns, bengals, dolphins])
 
     # We will learn about lambda later. For now know that this sorts the teams by name (A-Z)
     nfl.teams.sort(key=lambda x: x.name, reverse=False)
 
-    # returns the new list of nfl teams
     return nfl
 
 
@@ -41,7 +32,7 @@ def problem_2():
     # Use the results from problem 1
     nfl = problem_1()
 
-    # TODO: return just the Bengals schedule
-    for team in nfl.teams:
-        if team.name == "Bengals":
-            return team.schedule
+    # TODO: (3) return just the Bengals schedule
+    # for team in nfl.teams:
+    # some code here to filter the teams
+    # return a list containing the teams in the bengals schedule
